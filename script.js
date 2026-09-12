6011bfd6d1f5cd28ddc8d61fa9e6f8b32ab89d01
Imam Hasan <ihsakib54@gmail.com>
2026-09-12 16:13:23 +0600

    Update script.js

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ const SITE_CONFIG = {
   businessName: "Maria's Glad",
   messengerUrl: "https://m.me/mariyasglad",
   facebookUrl: "https://www.facebook.com/mariyasglad",
-  phone: "+880 1XXXXXXXXX",
-  address: "আপনার ব্যবসার ঠিকানা এখানে দিন",
+  phone: "+880 ----",
+  address: "----",
   currency: "৳"
 };
 
